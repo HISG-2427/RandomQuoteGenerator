00dcf7a5a56dc159dadcebd8e7ba456cf72406c2
Shivansh Ganguly <shivansh.ganguly.2007@gmail.com>
2025-09-05 15:33:25 +0530

    {

--- app.js
+++ app.js
@@ -16,9 +16,26 @@ const authors = [
     "Mahatma Gandhi",
     "Robert Frost"
 ]
+
+const cursorDot = document.querySelector("[data-cursor-dot]")
+const cursorOutline = document.querySelector("[data-cursor-outline]")
 const qt = document.querySelector(".quoteText");
 const at = document.querySelector(".authorText");
 const gb = document.querySelector(".generatebtn");
+
+window.addEventListener("mousemove", function (e) {
+    const posX = e.clientX;
+    const posY = e.clientY;
+
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
+    
+    cursorOutline.animate({
+        left: `${posX}px`,
+        top: `${posY}px`
+    }, { duration: 300, fill: "forwards"})
+});
+
 function generateQuotes() {
     var randInd = Math.floor(Math.random() * quotes.length);
     var randQuote = quotes[randInd];
